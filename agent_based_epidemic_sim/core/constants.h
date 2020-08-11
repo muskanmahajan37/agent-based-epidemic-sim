f7c515d0869cd630e733885fc2e5b5bb8cf244f7
groeney <groeney@google.com>
2020-08-10 22:54:12 -0700

    Deprecating micro_exposure_counts and replacing it with proximity_trace.
    micro_exposure_counts was the former way of representing micro exposures. It was a fixed length array where each index represented a distance (index = 1 == 1m) and the count at that index represented the total minutes spent at that distance between two hosts.
    
    proximity_trace is the new way of representing micro exposures. It is a fixed length array where each index represents a single "scan" at a fixed interval and the value at that index represents the distance between two hosts.
    
    PiperOrigin-RevId: 325960527

--- agent_based_epidemic_sim/core/constants.h
+++ agent_based_epidemic_sim/core/constants.h
@@ -6,6 +6,13 @@
 #include "agent_based_epidemic_sim/core/integral_types.h"
 
 namespace abesim {
+// Maximum numnber of proximity "scans" for a given proximity trace. This
+// effectively caps the maximum length of a given exposure. If a longer exposure
+// takes place, multiple Exposure objects should be created.
+inline const uint8 kMaxTraceLength = 20;
+
+// Number of days after initial infection that the host is considered to still
+// be infectious.
 inline const uint8 kMaxDaysAfterInfection = 14;
 
 // This table maps days since infection to an infectivity factor. It is computed
