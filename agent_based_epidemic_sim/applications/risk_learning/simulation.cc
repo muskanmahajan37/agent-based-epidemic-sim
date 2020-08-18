2eb5c20395d501680e282c94c7ba3e21ac66147c
mattr <mattr@google.com>
2020-08-17 22:59:53 -0700

    Read agents and locations from Riegeli encoded files.
    This is progress toward a main for risk score learning.
    - Remove dependence on home_work sim, which isn't very close to what
      we want to do.
    - Start adjusting the config proto toward the parameters we actually need.
    
    PiperOrigin-RevId: 327170413

--- agent_based_epidemic_sim/applications/risk_learning/simulation.cc
+++ agent_based_epidemic_sim/applications/risk_learning/simulation.cc
@@ -15,16 +15,26 @@
 #include "agent_based_epidemic_sim/applications/risk_learning/simulation.h"
 
 #include <memory>
+#include <vector>
 
+#include "absl/container/fixed_array.h"
+#include "absl/status/status.h"
 #include "absl/strings/string_view.h"
-#include "agent_based_epidemic_sim/applications/home_work/location_type.h"
-#include "agent_based_epidemic_sim/applications/home_work/simulation.h"
+#include "agent_based_epidemic_sim/agent_synthesis/population_profile.pb.h"
 #include "agent_based_epidemic_sim/applications/risk_learning/config.pb.h"
 #include "agent_based_epidemic_sim/applications/risk_learning/risk_score.h"
+#include "agent_based_epidemic_sim/core/agent.h"
 #include "agent_based_epidemic_sim/core/risk_score.h"
+#include "agent_based_epidemic_sim/core/simulation.h"
+#include "agent_based_epidemic_sim/port/time_proto_util.h"
+#include "riegeli/bytes/fd_reader.h"
+#include "riegeli/bytes/file_reader.h"
+#include "riegeli/records/record_reader.h"
 
 namespace abesim {
 namespace {
+
+using RiegeliBytesSource = riegeli::FdReader<>;
 
 class LearningRiskScoreGenerator : public RiskScoreGenerator {
  public:
@@ -42,18 +52,50 @@ class LearningRiskScoreGenerator : public RiskScoreGenerator {
 
 }  // namespace
 
-void RunSimulation(absl::string_view output_file_path,
-                   absl::string_view learning_output_base,
-                   const ContactTracingHomeWorkSimulationConfig& config,
-                   int num_workers) {
-  auto get_risk_score_generator = [&config](LocationTypeFn location_type) {
-    return absl::make_unique<LearningRiskScoreGenerator>(
-        config.tracing_policy(), std::move(location_type));
-  };
-  auto context = GetSimulationContext(config.home_work_config());
-  RunSimulation(output_file_path, learning_output_base,
-                config.home_work_config(), get_risk_score_generator,
-                num_workers, context);
+util::Status RunSimulation(const RiskLearningSimulationConfig& config,
+                           int num_workers) {
+  // Read in locations.
+  std::vector<std::unique_ptr<Location>> locations;
+  {
+    riegeli::RecordReader<RiegeliBytesSource> reader(
+        std::forward_as_tuple(config.location_file(), "r"));
+    LocationProto proto;
+    while (reader.ReadRecord(proto)) {
+      // TODO: Build locations from proto.
+    }
+    absl::Status status = reader.status();
+    if (!status.ok()) return status;
+    reader.Close();
+  }
+
+  // Read in agents.
+  std::vector<std::unique_ptr<Agent>> agents;
+  {
+    riegeli::RecordReader<RiegeliBytesSource> reader(
+        std::forward_as_tuple(config.agent_file(), "r"));
+    AgentProto proto;
+    while (reader.ReadRecord(proto)) {
+      // TODO: Build agents from proto.
+    }
+    absl::Status status = reader.status();
+    if (!status.ok()) return status;
+    reader.Close();
+  }
+
+  auto time_or = DecodeGoogleApiProto(config.init_time());
+  CHECK(time_or.ok());
+  const auto init_time = time_or.value();
+  auto step_size_or = DecodeGoogleApiProto(config.step_size());
+  CHECK(step_size_or.ok());
+  const auto step_size = step_size_or.value();
+
+  auto sim = num_workers > 1
+                 ? ParallelSimulation(init_time, std::move(agents),
+                                      std::move(locations), num_workers)
+                 : SerialSimulation(init_time, std::move(agents),
+                                    std::move(locations));
+  sim->Step(config.steps(), step_size);
+  return absl::OkStatus();
 }
 
 }  // namespace abesim
